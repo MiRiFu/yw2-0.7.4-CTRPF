fcdd1976049f7e081dd4dafa98f3354e6e15eee3
kani537 <94041681+kani537@users.noreply.github.com>
2023-03-29 20:32:04 +0900

    refactor

--- Includes/cheats.hpp
+++ Includes/cheats.hpp
@@ -9,9 +9,6 @@ namespace CTRPluginFramework
 {
     using StringVector = std::vector<std::string>;
 
-    void Cheat1(MenuEntry *entry);
-    void Test1(MenuEntry *entry);
-    void Search(MenuEntry *entry);
     void YokaiEditor(MenuEntry *entry);
     void Indicator(MenuEntry *entry);
 
